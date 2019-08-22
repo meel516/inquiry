d989edd8736546bac0d812b094ee5299196a1fb5
jtoepfer <jtoepfer@brookdale.com>
2019-08-22 15:40:47 -0500

    Updated note component to include proptype definitions

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import {Input, FormGroup, Label} from 'reactstrap';
+import React from 'react'
+import PropTypes from 'prop-types'
+import {Input, FormGroup, Label} from 'reactstrap'
 
-export default function Note(props) {
+export default function Note({label, id, name = id}) {
   return (
     <FormGroup>
-      <Label for={props.id}>{props.label}</Label>
-      <Input type="textarea" name={props.name} id={props.id} />
+      <Label for={id}>{label}</Label>
+      <Input type="textarea" name={name} id={id} />
     </FormGroup>
   )
+}
+
+Note.propTypes = {
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  name: PropTypes.string,
 }
