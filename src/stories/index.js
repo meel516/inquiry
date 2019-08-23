e490d432c526322707cde3281a3f020950246670
jtoepfer <jtoepfer@brookdale.com>
2019-08-23 10:19:34 -0500

    checked in storybook code

---
+++ src/stories/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
+
+import { Button, Welcome } from '@storybook/react/demo';
+
+// Inquiry Form Imports
+import AdditionalCareElements from '../Components/AdditionalCareElements';
+
+storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+
+storiesOf('Button', module)
+  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
+  .add('with some emoji', () => (
+    <Button onClick={action('clicked')}>
+      <span role="img" aria-label="so cool">
+        😀 😎 👍 💯
+      </span>
+    </Button>
+  ));
+
+// TODO: add simple react inquiry form stories here (https://www.learnstorybook.com/react/en/simple-component/)
