2468eec4dd9b49247a086cc9c6469c197f869225
jtoepfer <jtoepfer@brookdale.com>
2019-08-26 14:32:18 -0500

    fixed address unit test, not complete but fixed

--- src/__tests__/Address.test.js
+++ src/__tests__/Address.test.js
@@ -2,9 +2,15 @@ import React from 'react';
 import Address from '../components/Address';
 import { create } from 'react-test-renderer'
 
-describe('Address Test',()=>{
-    test('testing address class', () => {
-    let tree = create(<Address />)
-    expect(tree.toJSON()).toMatchSnapshot();
+describe('Address Test',()=> {
+    test('testing address component', () => {
+      const address = {
+
+      }
+
+      const mockOnChange = jest.fn();
+
+      let tree = create(<Address type="influencer" address={address} onChange={mockOnChange}/>)
+      expect(tree.toJSON()).toMatchSnapshot();
   })
 })
