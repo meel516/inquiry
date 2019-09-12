6a22dbacdc7fd36df32a492fc1893283e8b52e24
jtoepfer <joshua.toepfer@gmail.com>
2019-09-12 14:56:07 -0500

    updated configuration for nav bar and securing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,26 @@
 import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 import ScrollingLayoutManager from './pages/ScrollingLayoutManager';
-import SingePageLayoutManager from './pages/SingePageLayoutManager';
 import {Security, SecureRoute, ImplicitCallback} from '@okta/okta-react';
 import Home from './Home';
 
-const OKTA_CLIENTID = `${process.env.OKTA_CLIENTID}`;
+const config = {
+  issuer: `${process.env.REACT_APP_OKTA_URL}/oauth2/default`,
+  client_id: `${process.env.REACT_APP_OKTA_CLIENTID}`,
+  redirect_uri: `${window.location.origin}/implicit/callback`,
+}
 
 function App() {
   return (
-    <Router>
-      <Security issuer='https://brookdaledev.oktapreview.com/oauth2/default'
-                client_id={OKTA_CLIENTID}
-                redirect_uri={window.location.origin + '/implicit/callback'}>
-        <Route path='/' exact component={Home} />
-        <Route path='/implicit/callback' component={ImplicitCallback} />
-        <SecureRoute exact path="/inquiryForm" component={ScrollingLayoutManager} />
-        <SecureRoute exact path="/inquiryFormSingle" component={SingePageLayoutManager} />
-      </Security>
-    </Router>
+    <BrowserRouter>
+        <Security {...config}>
+          <SecureRoute path='/' exact component={Home} />
+          <Route path='/implicit/callback' component={ImplicitCallback} />
+          <SecureRoute exact path="/inquiryForm" component={ScrollingLayoutManager} />
+        </Security>
+    </BrowserRouter>
   );
 }
 
