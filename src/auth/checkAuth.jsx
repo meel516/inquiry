5aa495fb847e54048923b04e0309b76e14096197
jtoepfer <joshua.toepfer@gmail.com>
2019-09-13 08:39:24 -0500

    refactored where the auth check was located

--- src/auth/checkAuth.jsx
+++ src/auth/checkAuth.jsx
@@ -14,12 +14,13 @@
  * Helper function that watches the authenticate state, then applies it
  * as a boolean (authenticated) as well as attaches the userinfo data.
  */
-async function checkAuthentication() {
+async function checkAuthentication(callback) {
   const authenticated = await this.props.auth.isAuthenticated();
   if (authenticated !== this.state.authenticated) {
     if (authenticated && !this.state.userinfo) {
       const userinfo = await this.props.auth.getUser();
       this.setState({ authenticated, userinfo });
+      if (callback) callback(userinfo);
     } else {
       this.setState({ authenticated });
     }
