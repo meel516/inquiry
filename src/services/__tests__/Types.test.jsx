2763ae04093118e47c76660fd271b119062f4c2b
jtoepfer <joshua.toepfer@gmail.com>
2019-10-04 16:37:28 -0500

    broke down the different tests into describe sections

--- src/services/__tests__/Types.test.jsx
+++ src/services/__tests__/Types.test.jsx
@@ -2,62 +2,139 @@
 import { TestUtils } from '../../utils/test-utils'
 import { ObjectMappingService, Util as Utils } from "../Types"
 
-test('test phone number digits only', () => {
-    expect(Utils.stripPhoneFormatting('(414) 404-3453')).toEqual('4144043453')
+describe('phone manipulation', () => {
+
+    test('test phone number digits only', () => {
+        expect(Utils.stripPhoneFormatting('(414) 404-3453')).toEqual('4144043453')
+    })
+
+    test('verify creation of phone structure', () => {
+        expect(ObjectMappingService.createEmptyPhone()).toEqual({ number: "", type: "" })
+    })
+
 })
 
-test('verify creation of phone structure', () => {
-    expect(ObjectMappingService.createEmptyPhone()).toEqual({ number: "", type: "" })
+describe('address manipulation', () => {
+
+    test('verify contact does not have address', () => {
+        const contact = ObjectMappingService.createEmptyContact();
+        expect(Utils.hasAddress(contact)).toBeFalsy()
+    })
+
+    test('verify contact does have address', () => {
+        const contact = ObjectMappingService.createEmptyContact();
+        contact.address = ObjectMappingService.createEmptyAddress();
+        expect(Utils.hasAddress(contact)).toBeTruthy();
+    })
+
+    test('test address is not valid', () => {
+        expect(Utils.isAddressEmpty(null)).toBeFalsy();
+    })
+
 })
 
-test('verify contact does not have address', () => {
-    const contact = ObjectMappingService.createEmptyContact();
-    expect(Utils.hasAddress(contact)).toBeFalsy()
+describe('calling for tests', () => {
+
+    test('test Caller is calling for "MySelf" which is mapped to Prospect', () => {
+        const callingFor = "Myself";
+        expect(Utils.mapInquiryTypeValue(callingFor)).toBe('PROSP');
+    })
+
+    test('test Caller is not mapped to "MySelf" which is mapped to Influencer', () => {
+        const callingFor = '';
+        expect(Utils.mapInquiryTypeValue(callingFor)).toBe('INFLU')
+    })
+
+    test('test option not selected for Caller which defaults to Influencer', () => {
+        expect(Utils.mapInquiryTypeValue(null)).toBe('INFLU')
+        expect(Utils.mapInquiryTypeValue(undefined)).toBe('INFLU')
+        expect(Utils.mapInquiryTypeValue('')).toBe('INFLU')
+    })
+
 })
 
-test('verify contact does have address', () => {
-    const contact = ObjectMappingService.createEmptyContact();
-    contact.address = ObjectMappingService.createEmptyAddress();
-    expect(Utils.hasAddress(contact)).toBeTruthy();
+describe('test contact manipulation', () => {
+
+    test('test contact is empty', () => {
+        expect(Utils.isContactEmpty(null)).toBeTruthy();
+    })
+
+    test('test undefined contact is empty', () => {
+        expect(Utils.isContactEmpty(undefined)).toBeTruthy()
+    })
+
+    test('test empty contact still empty', () => {
+        const contact = ObjectMappingService.createEmptyContact();
+        expect(Utils.isContactEmpty(contact)).toBeTruthy()
+    })
+
+    test('test first name only still empty', () => {
+        const contact = ObjectMappingService.createEmptyContact();
+        TestUtils.addFirstName(contact)
+        expect(Utils.isContactEmpty(contact)).toBeTruthy()
+    })
+
 })
 
-test('test Caller is calling for "MySelf" which is mapped to Prospect', () => {
-    const callingFor = "Myself";
-    expect(Utils.mapInquiryTypeValue(callingFor)).toBe('PROSP');
+describe('test influencer mapping', () => {
+
+    test('test null influencer mapping', () => {
+        const contact = ObjectMappingService.createInfluencer(null)
+        expect(contact.influencerId).toBeUndefined()
+
+        expect(contact.firstName).toEqual('')
+        expect(contact.lastName).toEqual('')
+    })
+
+    test('test undefined influencer mapping', () => {
+        const contact = ObjectMappingService.createInfluencer(undefined)
+        expect(contact.influencerId).toBeUndefined()
+
+        expect(contact.firstName).toEqual('')
+        expect(contact.lastName).toEqual('')
+    })
+
+    test('test valid influencer (salesContact) mapping ', () => {
+        const influencer = {
+            influencerId: 1234567,
+            salesContact: {
+                contactId: 3456789,
+                masterId: 7867232,
+                firstName: 'Justin',
+                lastName: 'James',
+                age: 87,
+                address: {
+                    addressId: 3423132,
+                    addressType: 'HOME',
+                    primary: true,
+                    active: true,
+                },
+                phoneNumbers: [
+                    {
+                        onNationalDoNotCall: false,
+                        primary: true,
+                        phoneNumber: "4148977894",
+                        phoneType: "Home",
+                        phoneId: 12083324
+                    }
+                ]
+            }
+        }
+        const contact = ObjectMappingService.createInfluencer(influencer);
+
+        expect(contact.influencerId).toEqual(influencer.influencerId)
+        expect(contact.contactId).toEqual(influencer.salesContact.contactId)
+        expect(contact.address.addressId).toEqual(influencer.salesContact.address.addressId)
+    })
 })
 
-test('test Caller is not mapped to "MySelf" which is mapped to Influencer', () => {
-    const callingFor = '';
-    expect(Utils.mapInquiryTypeValue(callingFor)).toBe('INFLU')
-})
+describe('test prospect creation', () => {
 
-test('test option not selected for Caller which defaults to Influencer', () => {
-    expect(Utils.mapInquiryTypeValue(null)).toBe('INFLU')
-    expect(Utils.mapInquiryTypeValue(undefined)).toBe('INFLU')
-    expect(Utils.mapInquiryTypeValue('')).toBe('INFLU')
-})
-
-test('test address is not valid', () => {
-    expect(Utils.isAddressEmpty(null)).toBeFalsy();
-})
-
-test('test contact is empty', () => {
-    expect(Utils.isContactEmpty(null)).toBeTruthy();
-})
-
-test('test undefined contact is empty', () => {
-    expect(Utils.isContactEmpty(undefined)).toBeTruthy()
-})
-
-test('test empty contact still empty', () => {
-    const contact = ObjectMappingService.createEmptyContact();
-    expect(Utils.isContactEmpty(contact)).toBeTruthy()
-})
-
-test('test first name only still empty', () => {
-    const contact = ObjectMappingService.createEmptyContact();
-    TestUtils.addFirstName(contact)
-    expect(Utils.isContactEmpty(contact)).toBeTruthy()
+    test('create prospect request via undefined prospect', () => {
+        const prospect = ObjectMappingService.createProspectRequest(undefined);
+        expect(prospect).not.toBeNull();
+        
+    })
 })
 
 describe('test prospect needs', () => {
