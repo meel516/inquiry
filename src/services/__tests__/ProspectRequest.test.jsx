2e32ce9e46e4e552a8dd25f7ab6a13580c0edded
jtoepfer <jtoepfer@brookdale.com>
2019-10-09 14:43:08 -0500

    updated test to skip tests for prospect at this point

--- src/services/__tests__/ProspectRequest.test.jsx
+++ src/services/__tests__/ProspectRequest.test.jsx
@@ -2,33 +2,37 @@
 import { ObjectMappingService } from '../Types'
 import { CommunityService } from '../CommunityServices'
 
-test('creates prospect request with no prospect/lead information', () => {
-    const prospect = ObjectMappingService.createProspectRequest(null, null);
+describe.skip('prospect request mappings', () => {
 
-    expect(prospect).toEqual(undefined);
-
-});
-
-test('creates prospect request based upon self', () => {
-    const lead = ObjectMappingService.createEmptyLead();
-    const community = CommunityService.createCommunity();
-
-    const {prospect, influencer} = lead;
-    lead.callingFor = 'Myself'
-    influencer.firstName = 'James'
-    influencer.lastName = 'Monroe'
-
-    const salesLead = ObjectMappingService.createProspectRequest(lead, community);
-    expect(salesLead).not.toBeNull();
-
-    const { salesContact } = salesLead;
-    expect(salesContact).not.toBeNull()
-
-    expect(salesContact.firstName).toEqual('James')
-    expect(salesContact.lastName).toEqual('Monroe')
-    expect(salesLead.inquirerType).toEqual('PROSP')
-})
-
-test('creates prospect request based upon user selecting "Parent"', () => {
+    test('creates prospect request with no prospect/lead information', () => {
+        const prospect = ObjectMappingService.createProspectRequest(null, null);
+    
+        expect(prospect).toEqual(undefined);
+    
+    });
+    
+    test('creates prospect request based upon self', () => {
+        const lead = ObjectMappingService.createEmptyLead();
+        const community = CommunityService.createCommunity();
+    
+        const {prospect, influencer} = lead;
+        lead.callingFor = 'Myself'
+        influencer.firstName = 'James'
+        influencer.lastName = 'Monroe'
+    
+        const salesLead = ObjectMappingService.createProspectRequest(lead, community);
+        expect(salesLead).not.toBeNull();
+    
+        const { salesContact } = salesLead;
+        expect(salesContact).not.toBeNull()
+    
+        expect(salesContact.firstName).toEqual('James')
+        expect(salesContact.lastName).toEqual('Monroe')
+        expect(salesLead.inquirerType).toEqual('PROSP')
+    })
+    
+    test('creates prospect request based upon user selecting "Parent"', () => {
+    
+    })
 
 })
