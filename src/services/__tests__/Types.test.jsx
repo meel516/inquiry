09591b6af5fafbfee2fc12494e721cb24ffc1a6a
jtoepfer <jtoepfer@brookdale.com>
2019-10-09 14:44:31 -0500

    added influencer mapping tests

--- src/services/__tests__/Types.test.jsx
+++ src/services/__tests__/Types.test.jsx
@@ -33,7 +33,7 @@ describe('address manipulation', () => {
 
 })
 
-describe('calling for tests', () => {
+describe('Calling For field tests', () => {
 
     test('test Caller is calling for "MySelf" which is mapped to Prospect', () => {
         const callingFor = "Myself";
@@ -87,27 +87,45 @@ describe('test influencer mapping', () => {
     })
 
     test('test undefined influencer mapping', () => {
-        const contact = ObjectMappingService.createInfluencer(undefined)
-        expect(contact.influencerId).toBeUndefined()
+        const influencer = ObjectMappingService.createInfluencer(undefined)
+        expect(influencer.influencerId).toBeUndefined()
 
-        expect(contact.firstName).toEqual('')
-        expect(contact.lastName).toEqual('')
+        expect(influencer.firstName).toEqual('')
+        expect(influencer.lastName).toEqual('')
+        expect(influencer.gender).toEqual('')
+        expect(influencer.email).toEqual('')
+        expect(influencer.veteranStatus).toEqual('')
+        expect(influencer.phone).not.toBeNull()
+        expect(influencer.phone.number).toEqual('')
+        expect(influencer.phone.type).toEqual('')
+
     })
 
     test('test valid influencer (salesContact) mapping ', () => {
-        const influencer = {
-            influencerId: 1234567,
+        const salesInfluencer = {
+            primary: true,
+            active: true,
+            influencerId: 6063498,
+            leadId: 6861902,
+            username: "brookdale-sales-api",
             salesContact: {
-                contactId: 3456789,
-                masterId: 7867232,
-                firstName: 'Justin',
-                lastName: 'James',
-                age: 87,
+                firstName: "Bruce",
+                lastName: "Banner",
+                emailAddress: "bruce.banner@gmail.com",
+                contactId: 14104337,
+                masterId: 8575426,
+                medicaid: false,
+                medicare: false,
                 address: {
-                    addressId: 3423132,
-                    addressType: 'HOME',
                     primary: true,
-                    active: true,
+                    active: false,
+                    addressLine1: "2340 Acme Lane",
+                    addressLine2: "Suite #450",
+                    city: "Jacksonville",
+                    stateProv: "WI",
+                    zipPostalCode: "53214",
+                    addressType: "Home",
+                    addressId: 6258495
                 },
                 phoneNumbers: [
                     {
@@ -117,14 +135,45 @@ describe('test influencer mapping', () => {
                         phoneType: "Home",
                         phoneId: 12083324
                     }
-                ]
+                ],
             }
         }
-        const contact = ObjectMappingService.createInfluencer(influencer);
+        const influencer = ObjectMappingService.createInfluencer(salesInfluencer);
 
-        expect(contact.influencerId).toEqual(influencer.influencerId)
-        expect(contact.contactId).toEqual(influencer.salesContact.contactId)
-        expect(contact.address.addressId).toEqual(influencer.salesContact.address.addressId)
+        const { salesContact, salesContact: {address, phoneNumbers} } = salesInfluencer;
+
+        expect(influencer.influencerId).toEqual(salesInfluencer.influencerId)
+        expect(influencer.contactId).toEqual(salesContact.contactId)
+        expect(influencer.address.addressId).toEqual(address.addressId)
+        expect(influencer.firstName).toEqual(salesContact.firstName)
+        expect(influencer.lastName).toEqual(salesContact.lastName)
+        expect(influencer.contactId).toEqual(salesContact.contactId)
+        expect(influencer.masterId).toEqual(salesContact.masterId)
+        expect(influencer.phone.number).toEqual(phoneNumbers[0].phoneNumber)
+        expect(influencer.phone.type).toEqual(phoneNumbers[0].phoneType)
+    })
+})
+
+describe('test followup mapping', () => {
+
+    test('test edge case for follow up where undefined', () => {
+        const leadId = 1;
+        const community = {
+            uuid: "f5115381-b32b-4c04-8b10-c5173c1cd128",
+            communityId: 308282,
+            freeMeal: "2",
+            followupDate: "2019-10-16T17:44:46.987Z",
+            followUpAction: "5",
+            note: "Description",
+            startingPrice: 2540,
+            secondPersonFee: 500,
+            communityFee: 1850
+        }
+        const request = ObjectMappingService.createFollowupRequest(leadId, community)
+
+        expect(request.buildingId).toEqual(community.communityId)
+        expect(request.followUpActionId).toEqual(community.followUpAction)
+        expect(request.followUpDate).toEqual('2019-10-16T12:44:46.987-0500')
     })
 })
 
@@ -133,8 +182,15 @@ describe('test prospect creation', () => {
     test('create prospect request via undefined prospect', () => {
         const prospect = ObjectMappingService.createProspectRequest(undefined);
         expect(prospect).not.toBeNull();
-        
     })
+
+    test('create prospect request via fulled defined prospect', () => {
+
+    })
+})
+
+describe('test lead creation', () => {
+
 })
 
 describe('test prospect needs', () => {
@@ -282,3 +338,13 @@ describe('test prospect needs', () => {
 
     })
 })
+
+function createMockUser() {
+    return {
+        email: 'test1@brookdale.com',
+        name: 'Test Case',
+        username: 'testusername',
+        zone: 'America/Chicago',
+        locale: 'US',
+    }
+}
