efeeb5ff0a15b793d6c658276f80a1e968f8fb7a
Matthew Matthiessen <mmatthiessen@brookdale.com>
2019-10-11 12:23:44 -0500

    Fixing typo

--- src/pages/Footers/Footer.jsx
+++ src/pages/Footers/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer(props) {
   const version = `${process.env.REACT_APP_VERSION}`;
   return (
     <footer className="page-footer font-small">
-      <div className="footer-copyright text-center py-3">© 2019 Copyright: Brookdale Senior Living  <small>v{version}</small></div>
+      <div className="footer-copyright text-center py-3">© 2019 Copyright: Brookdale Senior Living  <small>{version}</small></div>
     </footer>
   )
 }
