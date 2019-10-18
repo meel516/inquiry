8b78f752a85d5f8cd99eaed33771b583b89f143f
jtoepfer <joshua.toepfer@gmail.com>
2019-10-18 15:50:08 -0500

    Added new component to display error messages at the top of the page

---
+++ src/components/DisplayErrors.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Alert, Row } from 'reactstrap';
+import PropTypes from 'prop-types'
+
+export default function DisplayErrors(props) {
+
+    if (props.readOnly) {
+        return (
+            <section className="errors">
+            </section>
+        )
+    }
+
+    if (props.status.error) {
+        return (
+            <section className="errors">
+                <div>Did not work.</div>
+            </section>
+        )
+    }
+
+    return (
+        <section className="errors">
+          <Row>
+            {!!props.error &&
+              <Alert color="danger">{props.message}</Alert>}
+          </Row>
+        </section>
+    )
+}
+
+DisplayErrors.propTypes = {
+    status: PropTypes.object.isRequired,
+    valid: PropTypes.bool,
+
+    readOnly: PropTypes.bool,
+    error: PropTypes.bool,
+  
+    message: PropTypes.string,
+  }
+  
+  DisplayErrors.defaultProps = {
+    readOnly: false,
+    error: false,
+  }
+  
