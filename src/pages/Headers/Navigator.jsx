79c6b93b8fae57dfa99da256c92de34cf01593a7
jtoepfer <joshua.toepfer@gmail.com>
2019-10-18 15:52:17 -0500

    changed navigator to be function and updated application name to be in the app.yaml/local

--- src/pages/Headers/Navigator.jsx
+++ src/pages/Headers/Navigator.jsx
@@ -1,33 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Navbar, NavbarBrand, Nav, NavItem } from 'reactstrap';
 
-export default class Navigator extends Component { 
-  render() {
-    return (
-      <Navbar color="light" light expand="md">
-        <NavbarBrand href="/">
-          <img
-            src="/bsl_logo.jpg"
-            width="253"
-            height="60"
-            className="d-inline-block align-top"
-            alt="Brookdale Senior Living"
-          />
-        </NavbarBrand>
-        <Nav md={5}>
+export default function Navigator(props) { 
+  const applicationName = `${process.env.REACT_APP_APPLICATION_NAME}`
+  return (
+    <Navbar color="light" light expand="md">
+      <NavbarBrand href="/">
+        <img
+          src="/bsl_logo.jpg"
+          width="253"
+          height="60"
+          className="d-inline-block align-top"
+          alt="Brookdale Senior Living"
+        />
+      </NavbarBrand>
+      <Nav md={5}>
+        <NavItem>
+          <h3>{applicationName}</h3>
+        </NavItem>
+      </Nav>
+      <Nav className="ml-auto" style={{paddingRight:"20px"}} navbar>
+      { 
+          props.authenticated !== null &&
           <NavItem>
-            <h3>Inquiry Form</h3>
+            <p>Welcome, {props.userinfo.given_name}!</p>
           </NavItem>
-        </Nav>
-        <Nav className="ml-auto" style={{paddingRight:"20px"}} navbar>
-        { 
-            this.props.authenticated !== null &&
-            <NavItem>
-              <p>Welcome, {this.props.userinfo.given_name}!</p>
-            </NavItem>
-          }
-        </Nav>
-      </Navbar>
-    );
-  }
+        }
+      </Nav>
+    </Navbar>
+  );
 }
+
