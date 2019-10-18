b0a792c93e1f71d45612d42ac51f6d870015ee6c
jtoepfer <joshua.toepfer@gmail.com>
2019-10-18 15:50:50 -0500

    Added proptypes to notes, and added rows, cols and default prop type values

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -7,14 +7,37 @@ export default function Note(props) {
   return (
     <FormGroup>
       <Label for={props.id} id={props.labelId} className="label-format">{props.label}</Label>
-      <Input type="textarea" name={props.name || `lead.notes.${props.id}`} id={props.id} onChange={props.onChange} onBlur={props.onBlur} />
+      <Input 
+        type="textarea" 
+        name={props.name || `lead.notes.${props.id}`} 
+        id={props.id} 
+        onChange={props.onChange} 
+        onBlur={props.onBlur} 
+        cols={props.cols}
+        rows={props.rows}
+        readOnly={props.isReadOnly}
+      />
       <ErrorMessage name={props.name || `lead.notes.${props.id}`} render={msg => <Alert color="danger" className="alert-smaller-size">{msg}</Alert>} />
     </FormGroup>
   )
 }
 
 Note.propTypes = {
-  label: PropTypes.string.isRequired,
-  onBlur: PropTypes.func.isRequired,
   id: PropTypes.string,
+  labelId: PropTypes.string.isRequired,
+
+  name: PropTypes.string,
+  label: PropTypes.string.isRequired,
+
+  onBlur: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired,
+
+  isReadOnly: PropTypes.bool,
+  rows: PropTypes.number,
+}
+
+Note.defaultProps = {
+  isReadOnly: false,
+  rows: 3,
+  cols: 10,
 }
