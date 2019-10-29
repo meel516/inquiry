83c3dd1c3276e99e4a2ff448f1bd68a1fd8b11d6
Casey Kerr <ckerr6@brookdale.com>
2019-10-29 17:30:52 -0500

    added dropdown types

---
+++ src/constants/dropdown-types/index.js
@@ -0,0 +1,13 @@
+export default {
+    stateProv: 'stateProv',
+    inquiryLeadSource: 'stateProv',
+    inquiryLeadSource: 'inquiryLeadSource',
+    phoneTypes: 'phoneTypes',
+    inquiryTypes: 'inquiryTypes',
+    veteranStatus: 'veteranStatus',
+    decisionTimeframe: 'decisionTimeframe',
+    interestReason: 'interestReason',
+    currentSituation: 'currentSituation',
+    careTypes: 'careTypes',
+    followUpActions: 'followUpActions'
+}
