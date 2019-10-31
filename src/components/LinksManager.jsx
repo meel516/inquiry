fa86ea3811e89b745d2a7c9b933c4b829d59ce56
jtoepfer <joshua.toepfer@gmail.com>
2019-10-31 17:03:48 -0500

    cleaned up constant links

--- src/components/LinksManager.jsx
+++ src/components/LinksManager.jsx
@@ -1,22 +1,7 @@
 import React from 'react';
 import { Col, ListGroup, ListGroupItem, Row } from 'reactstrap';
+import systemLinks from '../constants/system-links'
 
-const systemLinks = [
-  { name: "Advisor Admin Form", location: "http://bkdcontactcenter.host/advisoradmin/" },
-  { name: "CC Helpful Numbers", location: "http://teams.brookdaleliving.com/sites/sla/Quick%20Reference%20Guides/CC%20Helpful%20Numbers%202.12.19.docx" },
-  { name: "SMS", location: `${process.env.REACT_APP_SALES_URL}` },
-  { name: "Nurse on Call Locations", location: "http://www.nurseoncallfl.com/locations/" },
-  { name: "Entry Fee & CCRC", location: "http://teams.brookdaleliving.com/sites/sla/Quick%20Reference%20Guides/CCRC%20%20EF.docx" },
-  { name: "Entry Fee Communities", location: "http://teams.brookdaleliving.com/sites/sla/Quick%20Reference%20Guides/Entry%20Fee%20Communities.docx" },
-  { name: "Special Campaigns", location: "http://bkdcontactcenter.host/advisoradmin/" },
-  { name: "Terminology", location: "http://teams.brookdaleliving.com/sites/sla/Quick%20Reference%20Guides/Terminology.htm" },
-  { name: "Community Profiles", location: "https://bkdcontactcenter.host/community-profile-look-up/" },
-  { name: "CC Rate Finder", location: "http://edge.assisted.com/#/views/RateFinderUpdate/RateFinder?:iid=1" },
-  { name: "Communities with Villas", location: "http://teams.brookdaleliving.com/sites/sla/Quick%20Reference%20Guides/Communities%20that%20Offer%20Villas.docx" },
-  { name: "Free Meal Offer QRG", location: "http://teams.brookdaleliving.com/sites/sla/Special%20Campaigns%20%20Incentives/Free%20Meal%20Offer/Free%20Meal%20Offer%20QRG.docx" },
-  { name: "Special Event RSVP Guide", location: "http://teams.brookdaleliving.com/sites/sla/Special%20Campaigns%20%20Incentives/July%20Veterans%20Events%202018/Special%20Event%20RSVP%20Guide.docx" },
-  { name: "Aug Sept 2019 Promo QRG", location: "http://teams.brookdaleliving.com/sites/sla/Special%20Campaigns%20%20Incentives/2019%20Aug-Sept%20Free%20Month%20Promo/2019%20Aug-Sept%20Month%20Free%20Promo%20QRG.docx" },
-]
 
 export default class LinksManager extends React.Component {
   state = {
