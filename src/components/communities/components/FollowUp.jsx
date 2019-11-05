7067c69cb059b05146849514037ce89a889dcf0a
Alex Lurvey <alurvey@brookdale.com>
2019-11-05 15:23:36 -0600

    move community data fetching to root of community hierarchy; remove all build warnings

--- src/components/communities/components/FollowUp.jsx
+++ src/components/communities/components/FollowUp.jsx
@@ -1,44 +1,40 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import { FormGroup, Label } from 'reactstrap';
 import DateTimePicker from 'react-datetime-picker';
 
-class FollowUp extends React.Component {
-    state = {
-      followupDate: null,
-    }
-  
-    onScheduledDateChange = (date) => {
-      this.props.onFollowupDateChange(date)
-      this.setState({ followupDate: date })
-    }
-  
-    render() {
-      return (
-        <FormGroup>
-          <Label for="followupDate" className="label-format">Next Steps Date</Label>
-          <DateTimePicker
-            name="followupDate"
-            className="no-border form-control"
-            disableClock={true}
-            showWeekNumbers={true}
-            onChange={this.onScheduledDateChange}
-            value={this.props.community.scheduledDate || this.state.followupDate}
-            disabled={this.props.isReadOnly}
-          />
-        </FormGroup>
-      )
-    }
-  }
-  
-  FollowUp.propTypes = {
-    index: PropTypes.number.isRequired,
-    onFollowupDateChange: PropTypes.func.isRequired,
-    isReadOnly: PropTypes.bool,
-  }
-  
-  FollowUp.defaultProps = {
-    isReadOnly: false,
-  }
+const FollowUp = (props) => {
+  const [ followupDate, setFollowupDate ] = useState(null);
+  const { onFollowupDateChange } = props;
 
-  export default FollowUp;
+  const onScheduledDateChange = useCallback((date) => {
+    onFollowupDateChange(date);
+    setFollowupDate(date);
+  }, [setFollowupDate, onFollowupDateChange]);
+
+  return (
+    <FormGroup>
+      <Label for="followupDate" className="label-format">Next Steps Date</Label>
+      <DateTimePicker
+        name="followupDate"
+        className="no-border form-control"
+        disableClock={true}
+        showWeekNumbers={true}
+        onChange={onScheduledDateChange}
+        value={followupDate}
+        disabled={props.isReadOnly}
+      />
+    </FormGroup>
+  );
+}
+  
+FollowUp.propTypes = {
+  onFollowupDateChange: PropTypes.func.isRequired,
+  isReadOnly: PropTypes.bool,
+}
+
+FollowUp.defaultProps = {
+  isReadOnly: false,
+}
+
+export default FollowUp;
--- src/components/communities/components/FreeMeal.jsx
+++ src/components/communities/components/FreeMeal.jsx
@@ -3,26 +3,26 @@ import PropTypes from 'prop-types';
 import { Input, Label } from 'reactstrap';
 import freeMealListings from '../../../constants/free-meal-listings';
 
-function FreeMeal(props) {
-    return (
-      <React.Fragment>
-        <Label for="freeMeal" className="label-format">Does this Visit include a Free Meal?</Label>
-        <Input type="select" id="freeMeal" name="freeMeal" onChange={props.handleChange} disabled={props.isReadOnly}>
-          {freeMealListings.map((optn) => {
-            return <option key={optn.value} value={optn.label}>{optn.label}</option>
-          })}
-        </Input>
-      </React.Fragment>
-    )
-  }
-  
-  FreeMeal.propTypes = {
-    handleChange: PropTypes.func.isRequired,
-    isReadOnly: PropTypes.bool,
-  }
-  
-  FreeMeal.defaultProps = {
-    isReadOnly: false
-  }
+const freeMealOptions = freeMealListings.map((optn) => {
+  return <option key={optn.value} value={optn.label}>{optn.label}</option>
+});
 
-  export default FreeMeal;
+const FreeMeal = ({ isReadOnly, handleChange}) => (
+  <React.Fragment>
+    <Label for="freeMeal" className="label-format">Does this Visit include a Free Meal?</Label>
+    <Input type="select" id="freeMeal" name="freeMeal" onChange={handleChange} disabled={isReadOnly}>
+      {freeMealOptions}
+    </Input>
+  </React.Fragment>
+);
+  
+FreeMeal.propTypes = {
+  handleChange: PropTypes.func.isRequired,
+  isReadOnly: PropTypes.bool,
+}
+
+FreeMeal.defaultProps = {
+  isReadOnly: false
+}
+
+export default FreeMeal;
