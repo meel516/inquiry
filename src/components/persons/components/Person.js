c35ece49a79ee584daa22c93e31bf54d47d6de47
Alex Lurvey <alurvey@brookdale.com>
2019-11-13 11:42:14 -0600

    fix more indentation

--- src/components/persons/components/Person.js
+++ src/components/persons/components/Person.js
@@ -19,77 +19,77 @@ export const Person = ({ basePath, type, locked, onDuplicateFieldChange, onDupli
 
     useEffect(() => {
         getPhoneTypes()
-        .then((data) => setPhoneTypes(data));
+            .then((data) => setPhoneTypes(data));
     }, []);
 
     const phoneTypeOptions = useMemo(() => {
         return (phoneTypes || []).map(type => {
             return <option key={type.value} value={type.text}>{type.text}</option>
-            });
+        });
     }, [phoneTypes]);
 
     return (
         <>
-        <Row>
-            <Col>
-                <Label for={inputNames.firstName} className="label-format required-field">Name</Label>
-            </Col>
-        </Row>
-        <Row>
-            <Col>
-                <FormGroup>
-                    <Input 
-                        type='text'
-                        name={inputNames.firstName}
-                        placeholder='First Name'
-                        disabled={locked}
-                    />
-                </FormGroup>
-            </Col>
-            <Col>
-                <FormGroup>
-                    <Input
-                        type='text'
-                        name={inputNames.lastName}
-                        placeholder='Last Name'
-                        disabled={locked}
-                    />
-                </FormGroup>
-            </Col>
-        </Row>
-        <Row>
-            <Col>
-                <FormGroup>
-                    <Label for={inputNames.phone.number} className='label-format'>Phone</Label>
-                    <NumberFormat
-                        name={inputNames.phone.number}
-                        placeholder='Phone'
-                        onChange={onDuplicateFieldChange}
-                        onBlur={onDuplicateFieldBlur}
-                    />
-                </FormGroup>
-            </Col>
-            <Col>
-                <Label for={inputNames.phone.type} className='label-format'>Phone Type</Label>
-                <Select name={inputNames.phone.type} placeholder='Select One'>
-                    {phoneTypeOptions}
-                </Select>
-            </Col>
-        </Row>
-        <Row>
-            <Col>
-                <FormGroup>
-                    <Label for={inputNames.email} className='label-format'>Email</Label>
-                    <Input
-                        type='email'
-                        name={inputNames.email}
-                        onChange={onDuplicateFieldChange}
-                        onBlur={onDuplicateFieldBlur}
-                        placeholder='Email'
-                    />
-                </FormGroup>
-            </Col>
-        </Row>
+            <Row>
+                <Col>
+                    <Label for={inputNames.firstName} className="label-format required-field">Name</Label>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <FormGroup>
+                        <Input 
+                            type='text'
+                            name={inputNames.firstName}
+                            placeholder='First Name'
+                            disabled={locked}
+                        />
+                    </FormGroup>
+                </Col>
+                <Col>
+                    <FormGroup>
+                        <Input
+                            type='text'
+                            name={inputNames.lastName}
+                            placeholder='Last Name'
+                            disabled={locked}
+                        />
+                    </FormGroup>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <FormGroup>
+                        <Label for={inputNames.phone.number} className='label-format'>Phone</Label>
+                        <NumberFormat
+                            name={inputNames.phone.number}
+                            placeholder='Phone'
+                            onChange={onDuplicateFieldChange}
+                            onBlur={onDuplicateFieldBlur}
+                        />
+                    </FormGroup>
+                </Col>
+                <Col>
+                    <Label for={inputNames.phone.type} className='label-format'>Phone Type</Label>
+                    <Select name={inputNames.phone.type} placeholder='Select One'>
+                        {phoneTypeOptions}
+                    </Select>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <FormGroup>
+                        <Label for={inputNames.email} className='label-format'>Email</Label>
+                        <Input
+                            type='email'
+                            name={inputNames.email}
+                            onChange={onDuplicateFieldChange}
+                            onBlur={onDuplicateFieldBlur}
+                            placeholder='Email'
+                        />
+                    </FormGroup>
+                </Col>
+            </Row>
         </>
     )
 }
