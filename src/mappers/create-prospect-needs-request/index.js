4091bcd2163300701d56c88eac69507ea541473c
jtoepfer <jtoepfer@brookdale.com>
2019-11-20 15:19:01 -0600

    updated imports for prospect needs

--- src/mappers/create-prospect-needs-request/index.js
+++ src/mappers/create-prospect-needs-request/index.js
@@ -1,7 +1,3 @@
-import mapAdlNeedsToSales from '../map-adl-needs-to-sales'
-import mapMemoryConcernsToSales from '../map-memory-concerns-to-sales'
-import mapMobilityConcernsToSales from '../map-mobility-concerns-to-sales'
-import mapNutritionConcernsToSales from '../map-nutrition-concerns-to-sales'
 
 export default (leadId, { careType, adlNeeds, memoryConcerns, mobilityConcerns, nutritionConcerns }, user) => {
     if (leadId && careType) {
