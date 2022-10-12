de55bd850308677c288d180f1e5fba548823bf75
Marc Ramirez <mramirez55@brookdale.com>
2022-10-12 00:21:00 -0400

    TECP-6912: Fix data issues

--- src/pages/forms/InquiryMinForm.jsx
+++ src/pages/forms/InquiryMinForm.jsx
@@ -91,48 +91,76 @@ const InquiryForm = ({
     });
   }, [values, setFieldValue, validateForm]);
 
+  /**
+   * Ultimate callback for the "Edit Prospect" flow
+   * @type {(function(*): void)|*}
+   */
   const updateProspect = useCallback((lead) => {
     console.log(lead);
     const age = lead.prospect ? lead.prospect.age || '' : '';
-    let newProspect;
-    let newProspectIsLocked = isLocked;
+    let newLead;
     if (lead.prospect) {
-      newProspect = {...values.lead.prospect, ...lead.prospect, age};
+      // user selected a lead - let's rewrite our state with the new lead (and leave the prospect fields locked)
+      const newProspectIsLocked = isLocked;
+      newLead = {
+        ...values.lead,
+        ...lead,
+        adlNeeds: { ...values.lead.adlNeeds, ...lead.adlNeeds },
+        drivers: { ...values.lead.drivers, ...lead.driveers },
+        financialOptions: { ...values.lead.financialOptions, ...lead.financialOptions },
+        memoryConcerns: { ...values.lead.memoryConcerns, ...lead.memoryConcerns },
+        mobilityConcerns: { ...values.lead.mobilityConcerns, ...lead.mobilityConcerns },
+        notes: { ...values.lead.notes, ...lead.notes },
+        nutritionConcerns: { ...values.lead.nutritionConcerns, ...lead.nutritionConcerns },
+        prospect: { ...values.lead.prospect, ...lead.prospect, age },
+        secondPerson: { ...values.lead.secondPerson, ...lead.secondPerson },
+      };
+      setFieldValue('lead', newLead);
+      setProspectIsLocked(newProspectIsLocked);
     } else {
-      newProspectIsLocked = false;
-      newProspect = {
-        "firstName": '',
-        "lastName": '',
+      // user selected "None of these" - we need to clear out all the prospect information that was previously filled
+      // in (and unlock the prospect fields). I computed this by diffing the Formik state between "select influencer
+      // only" and "select influencer+lead, and then clear the influencer".
+      const newProspectIsLocked = false;
+      const newProspect = {
+        "firstName": "",
+        "lastName": "",
+        "gender": "",
         "email": "",
         "phone": {
-          "number": '',
-          "type": '',
-          "phoneId": undefined,
-          "primary": undefined,
+          "number": "",
+          "type": ""
         },
-        "veteranStatus": undefined,
-        "age": age,
-        "contactId": undefined,
-        "masterId": undefined,
-        "textOptInCheckbox": undefined,
-        "address": {
-          "line1": "",
-          "line2": "",
-          "city": "",
-          "state": "",
-          "zip": ""
-        }
+        "age": ""
       };
+      // 'undefined' here means remove from the object
+      newLead = {
+        ...values.lead,
+        influencer:              {
+          ...values.lead.influencer,
+          influencerId: undefined
+        },
+        callingFor:              "",
+        inquiryType:             0,
+        leadSource:              0,
+        leadSourceDetail:        0,
+        referralText:            "",
+        leadId:                  undefined,
+        leadTypeId:              undefined,
+        leadCareTypeId:          undefined,
+        veteranStatus:           undefined,
+        salesStage:              undefined,
+        buildingId:              undefined,
+        hasInfluencers:          undefined,
+        leadSourceDetailOptions: undefined,
+        prospect:                newProspect,
+      };
+      setFieldValue('lead', newLead);
+      setProspectIsLocked(newProspectIsLocked);
     }
-    const newLead = {
-      ...values.lead,
-      prospect: newProspect,
-    };
-    setFieldValue('lead', newLead);
-    setProspectIsLocked(newProspectIsLocked);
     validateForm({
       ...values,
-      lead:             newLead,
+      lead: newLead,
     });
   }, [values, isLocked, setFieldValue, validateForm]);
 
